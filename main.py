14a810aa386aea8620fa4e627c016caac3b13ac9
unknown <pydevcasts@gmail.com>
2026-08-13 20:00:36 +0330

    rename dl

---
+++ main.py
@@ -0,0 +1,30 @@
+# name = "ayoum nur"
+# for i in name:
+#     if i == 'z':
+#         print("Found it")
+#     else:
+#         print('Not Found')
+# ====================
+# for i in range(0,2):
+#     for j in range(0,2):
+#         for k in range(0,2):
+#             print(j)
+# =========================
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print("*", end = " ")
+#     print("")
+# ==========================
+# x = 0
+# while (x < 5):
+#     if x == 3:
+#         x = x + 1
+#         continue
+#     print(x)
+#     x = x + 1
+# =======================
+name = "ayoum"
+print(name[::-1])
+
+x = 10
+y = str(x)
